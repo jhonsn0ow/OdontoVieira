9ae8db8b532d2a0a37e104f4cc72834f1f523148
jhonsn0ow <105881944+jhonsn0ow@users.noreply.github.com>
2022-05-19 15:31:11 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
+function onScroll() {
+ 
+   showNavOnScroll()
+   showBackToTopButtonOnScroll()
+   
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+function showNavOnScroll() {
+    if (scrollY > 0) {
+        navigation.classList.add('scroll')
+        } else {
+            navigation.classList.remove('scroll')
+        }  
+}
+function showBackToTopButtonOnScroll() {
+    if (scrollY > 400) {
+        backToTopButton.classList.add('show')
+        } else {
+            backToTopButton.classList.remove('show')
+        }  
+}
+
+
+
+
+
+function openMenu() {
+document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 900,
+}).reveal(`
+ #home, 
+ #home img,
+ #home .stats,
+ #services,
+ #services .header,
+ #services .cards,
+ #services .butserv,
+ #sobre`)
